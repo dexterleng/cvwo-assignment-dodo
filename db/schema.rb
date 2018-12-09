cd0213275700fdac495a45b25c5c0241dc04053f
dexterleng <dexterlengcs@gmail.com>
2018-12-09 23:21:08 +0800

    Update database structure

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_09_101130) do
+ActiveRecord::Schema.define(version: 2018_12_09_151935) do
 
   create_table "tags", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_tags_on_name", unique: true
   end
 
   create_table "tags_tasks", id: false, force: :cascade do |t|
     t.integer "task_id", null: false
     t.integer "tag_id", null: false
+    t.integer "tasks_id"
+    t.integer "tags_id"
+    t.index ["tags_id"], name: "index_tags_tasks_on_tags_id"
+    t.index ["tasks_id"], name: "index_tags_tasks_on_tasks_id"
   end
 
   create_table "tasks", force: :cascade do |t|
