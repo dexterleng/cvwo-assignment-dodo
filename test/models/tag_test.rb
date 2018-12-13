5165d656446121e03fdb79345c2be39c5e3598a3
Dexter Leng <dexterlengcs@gmail.com>
2018-12-13 22:10:38 +0800

    Add tag unit tests

--- test/models/tag_test.rb
+++ test/models/tag_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
 class TagTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:michael)
+    @tag = tags(:school)
+  end
+
+  test "should be valid" do
+    assert @tag.valid?
+  end
+
+  test "tag name should be present" do
+    @tag.name = ""
+    assert_not @tag.valid?
+  end
+
+  test "downcases tag name" do
+    @tag = Tag.create(name: "YO")
+    assert_equal @tag.name, "yo"
+  end
 end
