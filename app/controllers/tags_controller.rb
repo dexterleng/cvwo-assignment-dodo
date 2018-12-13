b29d2c5dae1f9417beb0d75268546b31ee21c27f
Dexter Leng <dexterlengcs@gmail.com>
2018-12-13 23:51:58 +0800

    Show only tags with user tasks

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -2,6 +2,21 @@ class TagsController < ApplicationController
   before_action :logged_in_user, only: [:index]
 
   def index
-    @tags = Tag.all
+    @tasks = current_user.tasks.includes(:tags)
+    @tags_tasks = group_tasks_by_tag(@tasks)
+  end
+
+  private
+
+  # tag.name -> tasks
+  def group_tasks_by_tag(tasks)
+    tags_tasks = {}
+    tasks.each do |task|
+      task.tags.each do |tag|
+        tags_tasks[tag.name] = [] unless tags_tasks.key?(tag.name)
+        tags_tasks[tag.name] << task
+      end
+    end
+    return tags_tasks
   end
 end
