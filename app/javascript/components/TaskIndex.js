c9cd71f5a14cda95e7558e58565bd75e64ae0e46
dexterleng <dexterlengcs@gmail.com>
2019-01-01 00:57:51 +0800

    Filter list

--- app/javascript/components/TaskIndex.js
+++ app/javascript/components/TaskIndex.js
@@ -3,19 +3,43 @@ import TaskRow from './TaskRow';
 class TaskIndex extends React.Component {
   state = {
     tasks: [],
-    tags: null,
+    tags: [],
+    highlighted: false
   }
 
   static getDerivedStateFromProps(nextProps, prevState) {
     if (nextProps.tasks !== prevState.tasks){
-      return { tasks: Object.values(nextProps.tasks) };
+      return {
+        tasks: Object.values(nextProps.tasks),
+        tags: nextProps.tags,
+      };
     }
     return null;
+  }  
+
+  handleTagFilter = (tag) => {
+    this.setState({ highlighted: tag });
   }
 
   render() {
+    const taskFilter = this.state.highlighted === false ? (x) => true : (task) => task.tags.some(tag => tag.name === this.state.highlighted);
+    const tasks = this.state.tasks.filter(taskFilter);
     return (
       <React.Fragment>
+        <div>
+          <ul>
+            <li>
+              <FilterButton onClick={() => this.handleTagFilter(false)} highlighted={this.state.highlighted} id={false}>Show All</FilterButton>
+            </li>
+            {
+              this.state.tags.map((tag) => (
+                <li key={tag}>
+                  <FilterButton onClick={() => this.handleTagFilter(tag)} highlighted={this.state.highlighted} id={tag}>{tag}</FilterButton>
+                </li>
+              ))
+            }
+          </ul>
+        </div>
 
         <div>
           <table className="table table-striped table-responsive-md">
@@ -29,7 +53,7 @@ class TaskIndex extends React.Component {
             </thead>
             <tbody>
               {
-                this.state.tasks.map((task) => (
+                tasks.map((task) => (
                   <TaskRow key={task.id} task={task} current_user={this.props.current_user}/>
                 ))
               }
@@ -40,6 +64,12 @@ class TaskIndex extends React.Component {
       </React.Fragment>
     );
   }
+}
+
+const FilterButton = (props) => {  
+  return (
+    <button onClick={props.onClick} className={`btn ${ props.id === props.highlighted ? "btn-success" : "btn-warning" }`}>{props.children}</button>
+  );
 }
 
 export default TaskIndex
